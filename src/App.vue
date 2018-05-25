ee491c1c0e65e7aaf74b708d7606dac467af7f7c
codepleb <codepleb4@gmail.com>
2018-05-26 00:29:18 +0200

    Init layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <figure class="image is-128x128">
+          <img src="./assets/logo.png">
+        </figure>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Dashboard
+          </a>
+        </div>
+        <div class="navbar-end">
+          <a class="navbar-item">
+            subtangle index / milestone index
+          </a>
+        </div>
+      </div>
+    </nav>
+
+
+    <section class="hero is-primary" style="margin-top: 130px">
+      <div class="hero-body">
+        <p class="title">
+          inschpektor
+        </p>
+        <p class="subtitle">
+          Coming soon!
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
