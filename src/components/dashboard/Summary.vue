36e812b434a7178c18ae97f4d6549753c10e97f0
codepleb <codepleb4@gmail.com>
2018-07-23 15:07:11 +0200

    Fix summary displaying error

--- src/components/dashboard/Summary.vue
+++ src/components/dashboard/Summary.vue
@@ -4,11 +4,11 @@
       <div class="message-header">
         <p>Summary - Neighbors: {{neighbors.length}}</p>
 
-        <a v-if="showContent" class="button is-warning" :key="1">
+        <a v-if="showContent" class="button" :class="[issues(neighbors).length === 0 ? 'is-success' : 'is-warning']" :key="1">
           <i class="fas fa-window-minimize"></i>
         </a>
 
-        <a v-else class="button is-warning" :key="0">
+        <a v-else class="button"  :class="[issues(neighbors).length === 0 ? 'is-success' : 'is-warning']" :key="0">
           <i class="far fa-window-maximize"></i>
         </a>
 
