2e953d27b1a52128a1272225f6f84407c01a47c7
codepleb <codepleb4@gmail.com>
2018-07-24 19:55:36 +0200

    Change error to state the problem more precisely.

--- src/components/error/Error.vue
+++ src/components/error/Error.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <h1 class="title">Hi :)</h1>
-    <h2 class="subtitle">Looks like you didn't link your node yet. Please provide the IP / Domain below.</h2>
+    <div v-if="code === 'NODE_NOT_SET'">
+      <h2 class="subtitle">Looks like you didn't link your node yet. Please provide the IP / Domain below.</h2>
+    </div>
+
+    <div v-else>
+      <h2 class="subtitle">There appears to be problems reaching your node. You can link another IP down below, if it
+        changed.</h2>
+    </div>
 
     <input v-model="nodeIp" class="input" type="text" placeholder="IP / Domain of your node">
 
     <nav class="level">
       <div class="level-item has-text-centered">
         <div class="control">
-          <button class="button is-link" :class="{'is-loading': submitted}" @click="setHostNodeIp(nodeIp); submitted = true">Submit</button>
+          <button class="button is-link" :class="{'is-loading': submitted}"
+                  @click="setHostNodeIp(nodeIp); submitted = true">Submit
+          </button>
         </div>
       </div>
     </nav>
