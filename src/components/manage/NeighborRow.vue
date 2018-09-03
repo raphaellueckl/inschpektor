4d6ac3150a94363f16cba370dd72ecddc38d945c
codepleb <codepleb4@gmail.com>
2018-09-03 22:57:06 +0200

    Enable neighbor nick change

--- src/components/manage/NeighborRow.vue
+++ src/components/manage/NeighborRow.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="columns">
     <div class="column is-one-fifth">
-      {{neighbor.name ? neighbor.name : 'N/A'}}
+
+    <input v-model="neighbor.name" placeholder="N/A" class="input" type="text">
+    <span class="icon is-small is-right block" :key="0" v-on:click="submitNick(neighbor)">
+      <i class="fas fa-check"></i>
+    </span>
+
     </div>
     <div class="column is-two-fifths">
       {{neighbor.address}}
@@ -20,28 +25,31 @@
 </template>
 
 <script>
-  export default {
-    name: 'NeighborRow',
-    props: ['neighbor'],
-    data: () => {
-      return {
-        submitted: false
-      };
+export default {
+  name: "NeighborRow",
+  props: ["neighbor"],
+  data: () => {
+    return {
+      submitted: false
+    };
+  },
+  methods: {
+    remove() {
+      this.submitted = true;
+      this.$store.dispatch("removeNeighbor", this.neighbor);
     },
-    methods: {
-      remove() {
-        this.submitted = true;
-        this.$store.dispatch('removeNeighbor', this.neighbor);
-      }
+    submitNick(neighbor) {
+      console.log(neighbor);
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  .columns {
-    background-color: rgb(0, 159, 255, 0.85);
-    color: #fff;
-    margin-top: 4px;
-    border-radius: 4px;
-  }
+.columns {
+  background-color: rgb(0, 159, 255, 0.85);
+  color: #fff;
+  margin-top: 4px;
+  border-radius: 4px;
+}
 </style>
