5135ebc46838ac20c5669f02c884de56ef091347
codepleb <codepleb4@gmail.com>
2018-10-11 12:28:22 +0200

    Finalize spinner

--- src/components/NoContent.vue
+++ src/components/NoContent.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+  <div>
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="animated-info-text">
+      There might be a problem getting data from your node. If you access it remotely, please don't forget to allow at least the reading commands.
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,6 +25,7 @@
     width: 64px;
     height: 64px;
   }
+
   .lds-ellipsis div {
     position: absolute;
     top: 27px;
@@ -24,22 +35,27 @@
     background: #000;
     animation-timing-function: cubic-bezier(0, 1, 1, 0);
   }
+
   .lds-ellipsis div:nth-child(1) {
     left: 6px;
     animation: lds-ellipsis1 0.6s infinite;
   }
+
   .lds-ellipsis div:nth-child(2) {
     left: 6px;
     animation: lds-ellipsis2 0.6s infinite;
   }
+
   .lds-ellipsis div:nth-child(3) {
     left: 26px;
     animation: lds-ellipsis2 0.6s infinite;
   }
+
   .lds-ellipsis div:nth-child(4) {
     left: 45px;
     animation: lds-ellipsis3 0.6s infinite;
   }
+
   @keyframes lds-ellipsis1 {
     0% {
       transform: scale(0);
@@ -48,6 +64,7 @@
       transform: scale(1);
     }
   }
+
   @keyframes lds-ellipsis3 {
     0% {
       transform: scale(1);
@@ -56,6 +73,7 @@
       transform: scale(0);
     }
   }
+
   @keyframes lds-ellipsis2 {
     0% {
       transform: translate(0, 0);
@@ -63,5 +81,19 @@
     100% {
       transform: translate(19px, 0);
     }
+  }
+
+  .animated-info-text {
+    opacity: 0;
+    animation-name: fadein;
+    animation-timing-function: linear;
+    animation-duration: 1s;
+    animation-delay: 5s;
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
   }
 </style>
