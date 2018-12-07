a88468e28f0e7806db3a53ef08c91ff2c1ae35e7
codepleb <codepleb4@gmail.com>
2018-12-07 22:38:18 +0100

    Introduce rounded button component

---
+++ src/components/content/utility/RoundedButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="button is-rounded" :class="[buttonType]" :disabled="disabled" @click="click()">
+    {{text}}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'RoundedButton',
+    props: {
+      click: Function,
+      disabled: Boolean,
+      text: String,
+      type: String
+    },
+    data: function () {
+      return {
+        buttonType: this.type === 'ok' ? 'is-link' : ''
+      };
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
