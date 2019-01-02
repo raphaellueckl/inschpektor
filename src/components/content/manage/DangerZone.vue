a02bece112b5860a25ff676beb35a9655433080d
codepleb <codepleb4@gmail.com>
2019-01-02 01:44:03 +0100

    Modal for danger zone

--- src/components/content/manage/DangerZone.vue
+++ src/components/content/manage/DangerZone.vue
@@ -5,8 +5,18 @@
         <div class="tile is-parent is-vertical">
           <article class="tile is-child notification is-dark">
             <h1 class="title">Danger Zone</h1>
-            <a class="button is-danger is-rounded" @click="restartNode">Restart node</a>
-            <a class="button is-danger is-rounded" @click="resetDatabase">Reset inschpektor</a>
+
+            <div class="flex-container">
+              <RoundedButton class="centered" :click="restartNode" type="danger" spin="2000"
+                             modal-text="This action will restart your node. This will cause some connection issues from INSCHPEKTOR, but they will resolve automatically after the node is restarted.">
+                Restart Node
+              </RoundedButton>
+              <RoundedButton class="centered" :click="resetDatabase" type="danger" spin="2000"
+                             modal-text="Reset inschpektor to factory default mode. This will not affect your IOTA node, but INSCHPEKTOR needs to be reconfigured and the internal data will vanish. Do not forget to do a backup of your nicknames ('Save Nicknames') first!">
+                Reset Inschpektor
+              </RoundedButton>
+            </div>
+
           </article>
         </div>
       </div>
@@ -15,8 +25,13 @@
 </template>
 
 <script>
+  import RoundedButton from '../utility/RoundedButton';
+
   export default {
     name: 'DangerZone',
+    components: {
+      RoundedButton
+    },
     methods: {
       restartNode() {
         this.$store.dispatch('restartNode');
@@ -29,7 +44,13 @@
 </script>
 
 <style scoped>
-  .button.is-danger.is-rounded {
+  .centered {
     margin: 5px;
+  }
+
+  .flex-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
   }
 </style>
