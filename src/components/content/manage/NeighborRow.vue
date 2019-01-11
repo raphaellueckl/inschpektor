7fd70ccdc4ad23e83b4ef5b4f018b7d8826441e2
codepleb <raffi.lueckl@gmx.ch>
2019-01-11 02:40:51 +0100

    Greatly improve mobile view in manage neighbors

--- src/components/content/manage/NeighborRow.vue
+++ src/components/content/manage/NeighborRow.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="columns">
     <div class="column is-one-fifth">
+      <strong class="mobile-only">Nickname: </strong>
       <input v-model="neighbor.name" placeholder="Enter nickname..." class="input" type="text"
              @input="addNeighborNick(neighbor)">
     </div>
     <div class="column is-two-fifths">
-      {{neighbor.address}}
+      <strong class="mobile-only">IP-Address:</strong> {{neighbor.address}}
     </div>
     <div class="column">
-      {{neighbor.isActive === null ? 'N/A' : neighbor.isActive ? '✔️' : '❌' }}
+      <strong class="mobile-only">Active:</strong> {{neighbor.isActive === null ? 'N/A' : neighbor.isActive ? '✔️' : '❌' }}
     </div>
     <div class="column">
-      {{neighbor.isSynced === null ? 'N/A'
+      <strong class="mobile-only">Synced:</strong> {{neighbor.isSynced === null ? 'N/A'
       : neighbor.isSynced ? '✔' : '❌'}}️
     </div>
     <div class="column">
+      <strong class="mobile-only">Remove neighbor:</strong>
       <RoundedButton :click="remove" type="danger" spin="2000"
                      modal-text="This action will remove the selected neighbor from your node.">
         <font-awesome-icon icon="trash-alt"/>
@@ -63,5 +65,15 @@
 
   .center-checkmark {
     padding-top: 100%;
+  }
+
+  .mobile-only {
+    display: none;
+  }
+
+  @media screen and (max-width: 768px) {
+    .mobile-only {
+      display: unset;
+    }
   }
 </style>
