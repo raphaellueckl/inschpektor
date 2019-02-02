4a86c6f16e8c7a130850c4b6717bf8a143fa9154
codepleb <codepleb4@gmail.com>
2019-02-02 11:01:19 +0100

    Rearrange node info

--- src/components/content/NodeInfo.vue
+++ src/components/content/NodeInfo.vue
@@ -1,75 +1,77 @@
 <template>
-  <div class="tile is-ancestor">
-    <div class="tile is-vertical">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification">
-            <h1 class="title">{{hostNode}}</h1>
-            <div class="media-content">
-              <div class="content" v-if="nodeInfo">
-                <p>
-                  <strong>IRI Version: </strong>{{nodeInfo.appVersion}}
-                </p>
-                <p>
-                  <strong>Cores: </strong>{{nodeInfo.jreAvailableProcessors}}
-                </p>
-                <p>
-                  <strong>Ram: </strong>{{Math.round(nodeInfo.jreMaxMemory / 100000000) / 10}} GB
-                </p>
-                <p>
-                  <strong>Java Version: </strong>{{nodeInfo.jreVersion}}
-                </p>
-                <p>
-                  <strong>Neighbors: </strong>{{nodeInfo.neighbors}}
-                </p>
-                <p>
-                  <strong>Timestamp: </strong>{{nodeInfo.time === null ? null : nodeInfo.time | timespan}}
-                </p>
-                <p>
-                  <strong>Ping: </strong>{{nodeInfo.ping === null ? null : nodeInfo.ping}} ms
-                </p>
-                <p>
-                  <strong>RAM Usage: </strong>
-                  <progress class="progress is-success"
-                            :value="nodeInfo.jreFreeMemory"
-                            :max="nodeInfo.jreMaxMemory">
-                  </progress>
-                  <span> {{Math.round(nodeInfo.jreFreeMemory / 1000000)}} / {{Math.round(nodeInfo.jreMaxMemory / 1000000)}} MB</span>
-                </p>
-              </div>
-            </div>
-          </article>
-        </div>
+    <div class="tile is-ancestor">
+        <div class="tile is-vertical">
+            <div class="tile">
+                <div class="tile is-parent is-vertical">
+                    <article class="tile is-child notification">
+                        <p class="title">{{hostNode}}</p>
+                        <div class="media-content">
+                            <div class="content" v-if="nodeInfo">
+                                <p class="subtitle">Host-Server:</p>
+                                <p>
+                                    <strong>Ping: </strong>{{nodeInfo.ping === null ? null : nodeInfo.ping}} ms
+                                </p>
+                                <p>
+                                    <strong>Cores: </strong>{{nodeInfo.jreAvailableProcessors}}
+                                </p>
+                                <strong>RAM Usage: </strong>
+                                <span>{{Math.round(nodeInfo.jreFreeMemory / 1000000)}} / {{Math.round(nodeInfo.jreMaxMemory / 1000000)}} MB</span>
+                                <div>
+                                    <progress class="progress is-success"
+                                              :value="nodeInfo.jreFreeMemory"
+                                              :max="nodeInfo.jreMaxMemory">
+                                    </progress>
+                                </div>
+                                <p>
+                                    <strong>Java Version: </strong>{{nodeInfo.jreVersion}}
+                                </p>
 
-      </div>
+                                <br>
+                                <p class="subtitle">IRI:</p>
+                                <p>
+                                    <strong>IRI Version: </strong>{{nodeInfo.appVersion}}
+                                </p>
+                                <p>
+                                    <strong>Neighbors: </strong>{{nodeInfo.neighbors}}
+                                </p>
+                                <p>
+                                    <strong>Timestamp: </strong>{{nodeInfo.time === null ? null : nodeInfo.time |
+                                    timespan}}
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
-  import moment from 'moment';
+	import {mapGetters} from 'vuex';
+	import moment from 'moment';
 
-  export default {
-    name: 'NodeInfo',
-    computed: {
-      ...mapGetters(['nodeInfo', 'hostNode'])
-    },
-    created() {
-      this.$store.dispatch('fetchIriDetails');
-    },
-    filters: {
-      timespan: function (unixTimestampNum) {
-        return unixTimestampNum ? moment(unixTimestampNum).format('MM-DD-YYYY hh:mm A') : 'N/A';
-      }
-    }
-  };
+	export default {
+		name: 'NodeInfo',
+		computed: {
+			...mapGetters(['nodeInfo', 'hostNode'])
+		},
+		created() {
+			this.$store.dispatch('fetchIriDetails');
+		},
+		filters: {
+			timespan: function (unixTimestampNum) {
+				return unixTimestampNum ? moment(unixTimestampNum).format('MM-DD-YYYY hh:mm A') : 'N/A';
+			}
+		}
+	};
 </script>
 
 <style scoped>
-  progress.progress.is-success {
-    max-width: 300px;
-    display: inline-block;
-    margin-bottom: 0px;
-  }
+    progress.progress.is-success {
+        max-width: 300px;
+        display: inline-block;
+        margin-bottom: 0px;
+    }
 </style>
