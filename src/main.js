c856a0915554505955b09717092b28d33f83cdd1
codepleb <codepleb4@gmail.com>
2019-03-24 22:58:25 +0100

    remove dead code

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import {
-  initializeFirebase,
-  askForPermissioToReceiveNotifications
-} from './pushService';
+import { initializeFirebase } from './pushService';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -21,7 +18,6 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 Vue.config.productionTip = false;
 
 initializeFirebase();
-// askForPermissioToReceiveNotifications();
 
 library.add(faTrashAlt);
 library.add(faWindowMinimize);
--- src/pushService.js
+++ src/pushService.js
@@ -5,17 +5,3 @@ export const initializeFirebase = () => {
     messagingSenderId: '1008642879795'
   });
 };
-
-export const askForPermissioToReceiveNotifications = async () => {
-  try {
-    console.log('hi');
-    const messaging = firebase.messaging();
-    await messaging.requestPermission();
-    const token = await messaging.getToken();
-    console.log('Registered Token:', token);
-
-    return token;
-  } catch (error) {
-    console.error(error);
-  }
-};
