1cf704a60d98ae1f7f96862c2ca398bd9a309317
codepleb <codepleb4@gmail.com>
2019-04-13 22:45:46 +0200

    update firebase versions

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js');
 firebase.initializeApp({
   messagingSenderId: '1008642879795'
 });
