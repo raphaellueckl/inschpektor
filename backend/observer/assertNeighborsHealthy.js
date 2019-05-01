c63698c32845f787de12fc09db4ae18f64ff1831
codepleb <codepleb4@gmail.com>
2019-05-01 13:29:56 +0200

    fix healthy neighbor watcher

--- backend/observer/assertNeighborsHealthy.js
+++ backend/observer/assertNeighborsHealthy.js
@@ -8,9 +8,7 @@ const assertNeighborsHealthy = () => {
   if (
     isAllowedToSendNotification() &&
     NODE_STATE.currentNeighbors &&
-    NODE_STATE.currentNeighbors.filter(
-      n => !n.isFriendlyNode && n.milestone.latestSolidSubtangleMilestoneIndex
-    ).length > 0
+    NODE_STATE.currentNeighbors.filter(n => !n.isFriendlyNode).length > 0
   ) {
     NOTIFICATION_SERVICE.sendNotification('A neighbor turned foul!');
   }
