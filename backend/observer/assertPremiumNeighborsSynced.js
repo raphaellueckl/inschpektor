16eb394a7619f930a6ed6974e24f4e3eaed5c2d9
codepleb <codepleb4@gmail.com>
2019-05-01 13:05:39 +0200

    add untested neighbor sync assertion

--- backend/observer/assertPremiumNeighborsSynced.js
+++ backend/observer/assertPremiumNeighborsSynced.js
@@ -2,12 +2,26 @@ const axios = require('axios');
 
 const NODE_STATE = require('../state/node.state');
 const NOTIFICATION_SERVICE = require('../service/notification.service');
+const GLOBALS = require('../state/globals');
 const isAllowedToSendNotification = require('./gatekepper');
 
 const assertPremiumNeighborsSynced = () => {
-  //   if (isAllowedToSendNotification()) {
-  //     NOTIFICATION_SERVICE.sendNotification('A neighbor is out of sync!');
-  //   }
+  if (
+    isAllowedToSendNotification() &&
+    NODE_STATE.currentNeighbors &&
+    NODE_STATE.currentNeighbors
+      .filter(
+        n => n.milestone && n.milestone.latestSolidSubtangleMilestoneIndex
+      )
+      .filter(
+        n =>
+          n.latestSolidSubtangleMilestoneIndex >=
+          NODE_STATE.currentOwnNodeInfo.latestMilestoneIndex -
+            GLOBALS.MAX_MILESTONES_BEHIND_BEFORE_UNSYNCED
+      )
+  ) {
+    NOTIFICATION_SERVICE.sendNotification('A neighbor is out of sync!');
+  }
 };
 
 module.exports = assertPremiumNeighborsSynced;
