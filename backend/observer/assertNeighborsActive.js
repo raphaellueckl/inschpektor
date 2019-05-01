8173d30a50c3850f86e5e50666ade48061b15ea0
codepleb <codepleb4@gmail.com>
2019-05-01 13:29:45 +0200

    implement active neighbor watcher

--- backend/observer/assertNeighborsActive.js
+++ backend/observer/assertNeighborsActive.js
@@ -5,9 +5,13 @@ const NOTIFICATION_SERVICE = require('../service/notification.service');
 const isAllowedToSendNotification = require('./gatekepper');
 
 const assertNeighborsActive = () => {
-  //   if (isAllowedToSendNotification()) {
-  //     NOTIFICATION_SERVICE.sendNotification('A neighbor is out of sync!');
-  //   }
+  if (
+    isAllowedToSendNotification() &&
+    NODE_STATE.currentNeighbors &&
+    NODE_STATE.currentNeighbors.filter(n => !n.isActive).length > 0
+  ) {
+    NOTIFICATION_SERVICE.sendNotification('A neighbor turned inactive!');
+  }
 };
 
 module.exports = assertNeighborsActive;
