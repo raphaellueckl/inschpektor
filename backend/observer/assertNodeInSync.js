8ce8f47f2cd9dbab37109893bccba710d0bd1efe
codepleb <codepleb4@gmail.com>
2019-05-01 13:30:24 +0200

    fix assertNodeInSync

--- backend/observer/assertNodeInSync.js
+++ backend/observer/assertNodeInSync.js
@@ -5,10 +5,6 @@ const NOTIFICATION_SERVICE = require('../service/notification.service');
 const GLOBALS = require('../state/globals');
 const isAllowedToSendNotification = require('./gatekepper');
 
-// Only send a notification once a day
-const ONE_DAY_IN_MILIS = 1000 * 60 * 60 * 24;
-let lastTimeAlarmTriggered = new Date() - ONE_DAY_IN_MILIS;
-
 const assertNodeInSync = () => {
   if (
     isAllowedToSendNotification() &&
