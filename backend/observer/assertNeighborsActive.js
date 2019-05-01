8c2bdcdc2acd6d6d5f8e8b7122368d899f0cce14
codepleb <codepleb4@gmail.com>
2019-05-01 12:04:44 +0200

    add all notification assertiers (dummy)

---
+++ backend/observer/assertNeighborsActive.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+const NODE_STATE = require('../state/node.state');
+const NOTIFICATION_SERVICE = require('../service/notification.service');
+const isAllowedToSendNotification = require('./gatekepper');
+
+const assertNeighborsActive = () => {
+  //   if (isAllowedToSendNotification()) {
+  //     NOTIFICATION_SERVICE.sendNotification('A neighbor is out of sync!');
+  //   }
+};
+
+module.exports = assertNeighborsActive;
