295c27193abe61c539aceded5156de749338fd47
codepleb <codepleb4@gmail.com>
2019-05-01 13:21:59 +0200

    add healthy node check

--- backend/observer/assertNeighborsHealthy.js
+++ backend/observer/assertNeighborsHealthy.js
@@ -5,9 +5,15 @@ const NOTIFICATION_SERVICE = require('../service/notification.service');
 const isAllowedToSendNotification = require('./gatekepper');
 
 const assertNeighborsHealthy = () => {
-  //   if (isAllowedToSendNotification()) {
-  //     NOTIFICATION_SERVICE.sendNotification('A neighbor is out of sync!');
-  //   }
+  if (
+    isAllowedToSendNotification() &&
+    NODE_STATE.currentNeighbors &&
+    NODE_STATE.currentNeighbors.filter(
+      n => !n.isFriendlyNode && n.milestone.latestSolidSubtangleMilestoneIndex
+    ).length > 0
+  ) {
+    NOTIFICATION_SERVICE.sendNotification('A neighbor turned foul!');
+  }
 };
 
 module.exports = assertNeighborsHealthy;
