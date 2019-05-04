64b895d98450970276a5dc2f2c448bf5692c23c1
codepleb <codepleb4@gmail.com>
2019-05-04 13:41:59 +0200

    Add new logo to about page

--- src/components/content/about/About.vue
+++ src/components/content/about/About.vue
@@ -1,38 +1,43 @@
 <template>
   <div>
-    <img src="../../../assets/logo.png" />
-    <p><strong>Installed Version:</strong> {{ installedVersion }}</p>
-    <p
-      v-if="latestInschpektorVersion > installedVersion"
-      class="update-available"
-    >
-      <strong>🌟 Update available!</strong> Version
+    <!-- <img src="../../../assets/logo.png" /> -->
+    <div class="logo-holder">
+      <div class="logo-item">
+        <img src="@/assets/logo_300.png" style="width:150px; height:150px;">
+      </div>
+      <div class="logo-item">
+        <span>inschpektor</span>
+      </div>
+    </div>
+
+    <p>
+      <strong>Installed Version:</strong>
+      {{ installedVersion }}
+    </p>
+    <p v-if="latestInschpektorVersion > installedVersion" class="update-available">
+      <strong>🌟 Update available!</strong>
+      Version
       {{ latestInschpektorVersion }} is ready to download! See here how to
       update:
       <a
         href="https://github.com/codepleb/inschpektor/blob/master/readme.md"
         target="_blank"
-        >Update manual</a
-      >
+      >Update manual</a>
       🌟
     </p>
-    <br />
+    <br>
     <p>
       This is an open source app, that can be found @
-      <a href="https://github.com/codepleb/inschpektor">Github</a>.
+      <a
+        href="https://github.com/codepleb/inschpektor"
+      >Github</a>.
     </p>
-    <p>
-      The application is based on Vuejs.
-    </p>
-    <br />
-    <p>
-      If you would like to contribute: I'd love to get your pullrequests. :)
-    </p>
-    <p>
-      Hit me up on telegram: @codepleb
-    </p>
-    <br />
-    <br />
+    <p>The application is based on Vuejs.</p>
+    <br>
+    <p>If you would like to contribute: I'd love to get your pullrequests. :)</p>
+    <p>Hit me up on telegram: @codepleb</p>
+    <br>
+    <br>
     <h3 class="title is-3">Donations</h3>
     <p>
       If you like this app and you want to support me, use the following IOTA
@@ -43,11 +48,14 @@
       address="IZ99FQWJUQJCDLBSUOEDZIVROSARFYLXJWPZNAXQPCCGAHCUWKRE9RNHMGNHZTAIZHPUDV9HMSPNJLYSXPNEPTRHUW"
     />
 
-    <br />
+    <br>
     <p>
       If you prefer another coin, you can have a look at the other options over
       @
-      <a target="_blank" href="https://codepleb.net/donations">codepleb.net</a>
+      <a
+        target="_blank"
+        href="https://codepleb.net/donations"
+      >codepleb.net</a>
     </p>
   </div>
 </template>
@@ -76,5 +84,21 @@ export default {
 .update-available {
   background: hsl(141, 71%, 70%);
   border-radius: 1vh;
+}
+
+.logo-holder {
+  /* display: flex; */
+  /* align-items: center; */
+}
+
+.logo-item {
+  margin: 5px;
+  color: black;
+  font-size: 40px;
+  font-family: monospace;
+}
+
+.logo-item:last-child {
+  margin-bottom: 20px;
 }
 </style>
