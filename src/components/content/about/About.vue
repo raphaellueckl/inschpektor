ed6b28b3aa3d30bff4a1a390b20dc82bcd8ed389
codepleb <codepleb4@gmail.com>
2019-05-14 22:28:51 +0200

    Append to donation section

--- src/components/content/about/About.vue
+++ src/components/content/about/About.vue
@@ -48,14 +48,21 @@
       address="IZ99FQWJUQJCDLBSUOEDZIVROSARFYLXJWPZNAXQPCCGAHCUWKRE9RNHMGNHZTAIZHPUDV9HMSPNJLYSXPNEPTRHUW"
     />
 
-    <br>
+    <h5 class="title is-5">Other Coins</h5>
     <p>
-      If you prefer another coin, you can have a look at the other options over
-      @
-      <a
-        target="_blank"
-        href="https://codepleb.net/donations"
-      >codepleb.net</a>
+      <strong>BTC:</strong> 32o6kbgZqJ9nhT2aRsfEGcMtpThJ2za89T
+    </p>
+    <p>
+      <strong>LTC:</strong> LgfSNjqVpXFj9xN8ZWPR9qTZQJpcRRNXRZ
+    </p>
+    <p>
+      <strong>XMR:</strong> 4GdoN7NCTi8a5gZug7PrwZNKjvHFmKeV11L6pNJPgj5QNEHsN6eeX3DaAQFwZ1ufD4LYCZKArktt113W7QjWvQ7CWAjwVCr95LATLKqtxr
+    </p>
+    <p>
+      <strong>XLM:</strong> GA5XIGA5C7QTPTWXQHY6MCJRMTRZDOSHR6EFIBNDQTCQHG262N4GGKTM
+    </p>
+    <p>
+      <strong>XRP:</strong> rLHzPsX6oXkzU2qL12kHCH8G8cnZv1rBJh
     </p>
   </div>
 </template>
