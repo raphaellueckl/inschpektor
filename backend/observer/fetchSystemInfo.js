618b354596b84ed1c837e4b22406ecd684d6db26
Raphael Lückl <raphaelluckl@Raphaels-MBP.fritz.box>
2019-10-23 23:01:12 +0200

    Add all system info

--- backend/observer/fetchSystemInfo.js
+++ backend/observer/fetchSystemInfo.js
@@ -3,31 +3,22 @@ const si = require('systeminformation');
 const NODE_STATE = require('../state/node.state');
 
 const fetchSystemInfo = async () => {
-    // si.currentLoad()
-    // .then(data => console.log(`${data.currentload} / ${data.currentload_idle}`))
-    // .catch(error => console.error(error));
-    
-    // si.processes()
-    // .then(data => console.log(`${data.all}`)) // Number of all Processes
-    // .catch(error => console.error(error));
-    
-    // si.disksIO()
-    // .then(data => console.log(`rw_dsk: ${data.rIO_sec} / ${data.wIO_sec}`)) // read and write on disks per sec
-    // .catch(error => console.error(error));
-    
-    // // si.fsStats()
-    // // .then(data => console.log(`rw: ${data.rx_sec} / ${data.wx_sec}`)) // read and write on disks per sec
-    // // .catch(error => console.error(error));
-    
-    // si.networkInterfaces()
-    // .then(data => (`speedy: ${data[0].speed}}`)) // read and write on disks per sec
-    // .catch(error => console.error(error));
-    
-    si.networkStats()
-    .then(data => data.forEach(d => console.log(`speed: ${d.rx_sec / 1024 / 1024 * 8} / ${d.tx_sec / 1024 / 1024 * 8}`))) // transferred mb per second, receive / send
-    .catch(error => console.error(error));
+  const { currentload } = await si.currentLoad();
+  const { all } = await si.processes();
+  const { tIO_sec } = await si.disksIO();
+  const { rx_sec, tx_sec } = await si.networkInterfaces();
 
-    console.log('\n###\n')
+  const combinedData = {
+    cpuLoad: currentload,
+    allProcesses: all,
+    diskIO: tIO_sec,
+    networkIO: {
+      upload: (rx_sec / 1024 / 1024).toFixed(2), // Mbit/s
+      download: (tx_sec / 1024 / 1024).toFixed(2) // Mbit/s
+    }
+  };
+  NODE_STATE.systemInfo.unshift(combinedData);
+  NODE_STATE.systemInfo = [combinedData, ...NODE_STATE.systemInfo.slice(0, 99)];
 };
 
 module.exports = fetchSystemInfo;
