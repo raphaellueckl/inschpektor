5644a4058b94a1480ee9b28d904ca33100345c83
codepleb <codepleb4@gmail.com>
2019-11-03 10:24:38 +0100

    Reenable charts

--- src/components/content/NodeInfo.vue
+++ src/components/content/NodeInfo.vue
@@ -1,116 +1,109 @@
 <template>
-  <div class="tile is-ancestor">
-    <div></div>
-    <div>
-      <trend-chart class="chart" :datasets="systemInfo_networkIO" :grid="grid_" :labels="labels_"></trend-chart>
-    </div>
-  </div>
-  <!-- <div class="tile is-vertical">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification">
-            <p class="title">{{ hostNode ? hostNode : 'N/A' }}</p>
-            <div class="media-content">
-              <div class="content" v-if="nodeInfo">
-                <p class="subtitle">Host-Server:</p>
-                <p>
-                  <strong>Ping:</strong>
-                  {{ nodeInfo.ping === null ? null : nodeInfo.ping }} ms
-                </p>
-                <p>
-                  <strong>Cores:</strong>
-                  {{ nodeInfo.jreAvailableProcessors }}
-                </p>
-                <strong>RAM Usage:</strong>
-                <span>
-                  {{ Math.round(nodeInfo.jreFreeMemory / 1000000) }} /
-                  {{ Math.round(nodeInfo.jreMaxMemory / 1000000) }} MB
-                </span>
-                <div>
-                  <progress
-                    class="progress is-success"
-                    :value="nodeInfo.jreFreeMemory"
-                    :max="nodeInfo.jreMaxMemory"
-                  ></progress>
-                </div>
-                <p>
-                  <strong>Java Version:</strong>
-                  {{ nodeInfo.jreVersion }}
-                </p>
+  <div class="tile is-vertical">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification">
+          <p class="title">{{ hostNode ? hostNode : 'N/A' }}</p>
+          <div class="media-content">
+            <div class="content" v-if="nodeInfo">
+              <p class="subtitle">Host-Server:</p>
+              <p>
+                <strong>Ping:</strong>
+                {{ nodeInfo.ping === null ? null : nodeInfo.ping }} ms
+              </p>
+              <p>
+                <strong>Cores:</strong>
+                {{ nodeInfo.jreAvailableProcessors }}
+              </p>
+              <strong>RAM Usage:</strong>
+              <span>
+                {{ Math.round(nodeInfo.jreFreeMemory / 1000000) }} /
+                {{ Math.round(nodeInfo.jreMaxMemory / 1000000) }} MB
+              </span>
+              <div>
+                <progress
+                  class="progress is-success"
+                  :value="nodeInfo.jreFreeMemory"
+                  :max="nodeInfo.jreMaxMemory"
+                ></progress>
+              </div>
+              <p>
+                <strong>Java Version:</strong>
+                {{ nodeInfo.jreVersion }}
+              </p>
 
-                <br />
-                <p class="subtitle">IRI:</p>
-                <p>
-                  <strong>IRI Version:</strong>
-                  {{ nodeInfo.appVersion }}
-                </p>
-                <p>
-                  <strong>Neighbors:</strong>
-                  {{ nodeInfo.neighbors }}
-                </p>
-                <p>
-                  <strong>Timestamp:</strong>
-                  {{
-                  nodeInfo.time === null ? null : nodeInfo.time | timespan
-                  }}
-                </p>
+              <br />
+              <p class="subtitle">IRI:</p>
+              <p>
+                <strong>IRI Version:</strong>
+                {{ nodeInfo.appVersion }}
+              </p>
+              <p>
+                <strong>Neighbors:</strong>
+                {{ nodeInfo.neighbors }}
+              </p>
+              <p>
+                <strong>Timestamp:</strong>
+                {{
+                nodeInfo.time === null ? null : nodeInfo.time | timespan
+                }}
+              </p>
 
-                <div>
-                  <strong>CPU Usage (%):</strong>
-                </div>
-                <div>
-                  <trend-chart
-                    class="chart"
-                    :datasets="systemInfo_cpu"
-                    :grid="grid_"
-                    :labels="labels_"
-                    :min="min"
-                    :max="max"
-                  ></trend-chart>
-                </div>
+              <div>
+                <strong>CPU Usage (%):</strong>
+              </div>
+              <div>
+                <trend-chart
+                  class="chart"
+                  :datasets="systemInfo_cpu"
+                  :grid="grid_"
+                  :labels="labels_"
+                  :min="min"
+                  :max="max"
+                ></trend-chart>
+              </div>
 
-                <div>
-                  <strong>Running Processes (#):</strong>
-                </div>
-                <div>
-                  <trend-chart
-                    class="chart"
-                    :datasets="systemInfo_runningProcesses"
-                    :grid="grid_"
-                    :labels="labels_"
-                  ></trend-chart>
-                </div>
+              <div>
+                <strong>Running Processes (#):</strong>
+              </div>
+              <div>
+                <trend-chart
+                  class="chart"
+                  :datasets="systemInfo_runningProcesses"
+                  :grid="grid_"
+                  :labels="labels_"
+                ></trend-chart>
+              </div>
 
-                <div>
-                  <strong>Disk I/O (KB/s):</strong>
-                </div>
-                <div>
-                  <trend-chart
-                    class="chart"
-                    :datasets="systemInfo_diskIO"
-                    :grid="grid_"
-                    :labels="labels_"
-                  ></trend-chart>
-                </div>
+              <div>
+                <strong>Disk I/O (KB/s):</strong>
+              </div>
+              <div>
+                <trend-chart
+                  class="chart"
+                  :datasets="systemInfo_diskIO"
+                  :grid="grid_"
+                  :labels="labels_"
+                ></trend-chart>
+              </div>
 
-                <div>
-                  <strong>Network I/O (MB/s):</strong>
-                </div>
-                <div>
-                  <trend-chart
-                    class="chart"
-                    :datasets="systemInfo_networkIO"
-                    :grid="grid_"
-                    :labels="labels_"
-                  ></trend-chart>
-                </div>
+              <div>
+                <strong>Network I/O (MB/s):</strong>
+              </div>
+              <div>
+                <trend-chart
+                  class="chart"
+                  :datasets="systemInfo_networkIO"
+                  :grid="grid_"
+                  :labels="labels_"
+                ></trend-chart>
               </div>
             </div>
-          </article>
-        </div>
+          </div>
+        </article>
       </div>
     </div>
-  </div>-->
+  </div>
 </template>
 
 <script>
