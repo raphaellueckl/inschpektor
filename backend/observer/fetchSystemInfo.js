18648ed68cf5798e78c1a0f5afba7dd5a304cf4f
codepleb <codepleb4@gmail.com>
2019-11-10 12:38:40 +0100

    Add comment to everything received

--- backend/observer/fetchSystemInfo.js
+++ backend/observer/fetchSystemInfo.js
@@ -3,12 +3,12 @@ const si = require('systeminformation');
 const NODE_STATE = require('../state/node.state');
 
 const fetchSystemInfo = async () => {
-  const { currentload } = await si.currentLoad();
-  const { running } = await si.processes();
-  const { tIO_sec } = await si.disksIO();
+  const { currentload } = await si.currentLoad(); // CPU load in %
+  const { running } = await si.processes(); // Number of running processes
+  const { tIO_sec } = await si.disksIO(); // Number of Disk IO r/w
   const network = await si.networkStats();
-  const receiving = Number((network[0].rx_sec / 1024 / 1024).toFixed(1));
-  const sending = Number((network[0].tx_sec / 1024 / 1024).toFixed(1));
+  const receiving = Number((network[0].rx_sec / 1024 / 1024).toFixed(1)); // Network download in mbps (byte)
+  const sending = Number((network[0].tx_sec / 1024 / 1024).toFixed(1)); // Network upload in mbps (byte)
   const combinedData = {
     cpuLoad: currentload,
     runningProcesses: running,
