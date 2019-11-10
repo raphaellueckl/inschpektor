6eef1bc1ed4a195f2367b96d7c3cdf13985d8db8
codepleb <codepleb4@gmail.com>
2019-11-10 22:26:42 +0100

    Change disk io algorithm and refactor code

--- backend/observer/fetchSystemInfo.js
+++ backend/observer/fetchSystemInfo.js
@@ -3,19 +3,17 @@ const si = require('systeminformation');
 const NODE_STATE = require('../state/node.state');
 
 const fetchSystemInfo = async () => {
-  const { currentload } = await si.currentLoad(); // CPU load in %
-  const { running } = await si.processes(); // Number of running processes
-  const { tIO_sec } = await si.disksIO(); // Number of Disk IO r/w
+  const { currentload } = await si.currentLoad();
+  const { running } = await si.processes();
   const network = await si.networkStats();
-  const receiving = Number((network[0].rx_sec / 1024 / 1024).toFixed(1)); // Network download in mbps (byte)
-  const sending = Number((network[0].tx_sec / 1024 / 1024).toFixed(1)); // Network upload in mbps (byte)
+  const { tx_sec } = await si.fsStats(); // Combined disk IO in Bytes per second
   const combinedData = {
-    cpuLoad: currentload,
-    runningProcesses: running,
-    diskIO: tIO_sec,
+    cpuLoad: currentload, // CPU load in %
+    runningProcesses: running, // Number of running processes
+    diskIO: Number((tx_sec / 1024 / 1024).toFixed(2)), // Combined disk IO in MB/s per second
     networkIO: {
-      upload: sending,
-      download: receiving
+      upload: Number((network[0].tx_sec / 1024 / 1024).toFixed(1)), // Network upload in mbps (byte)
+      download: Number((network[0].rx_sec / 1024 / 1024).toFixed(1)) // Network download in mbps (byte)
     }
   };
   NODE_STATE.systemInfo.unshift(combinedData);
